1656d914791d845b865499bf25984d076e337c98
Jose Carlos Nuñez Valverde <josek21nunez@gmail.com>
2018-05-20 06:24:12 -0600

    Creación de usuario
    SingIn

--- odyssey_s.cpp
+++ odyssey_s.cpp
@@ -18,7 +18,9 @@ QString xml;
 QString xml2;
 QString folder1 = "/home/josek/FolderOdyssey";
 QString folder2 =  "/home/josek/FolderOdyssey/AudioFile";
+std::string user = "/home/josek/FolderOdyssey/Users.json";
 
+std::ofstream ficheroSalida;
 
 int j=0;
 Odyssey_S::Odyssey_S(QWidget *parent) :
@@ -54,27 +56,17 @@ Odyssey_S::~Odyssey_S()
 void Odyssey_S::createFile(){
    if (QDir(folder1).exists() == false){
        QDir().mkdir(folder1);
-
-       std::string user = folder1.append("/Users.json").toStdString();
-
        std::ofstream userFile(user);
-
-
-       userFile << "User 1" << std::endl;
-       userFile << "User 2" << std::endl;
-       userFile << "User 3" << std::endl;
        userFile.close();
 
-       qDebug()<<"FolderOdyssey created";
+       qDebug()<<"FolderOdyssey created with Users.json";
 
        if (QDir(folder2).exists() == false){
            QDir().mkdir(folder2);
            std::string metadata = folder2.append("/Metadata.json").toStdString();
            std::ofstream metadataFile(metadata);
-           metadataFile << "metadata 1" << std::endl;
-           metadataFile << "metadata 2" << std::endl;
            metadataFile.close();
-           qDebug()<<"AudioFile created";
+           qDebug()<<"AudioFile created with Metadata.json";
        }
    }
 
@@ -131,6 +123,43 @@ void Odyssey_S::leer_socketLogin() {       //Recibe los datos del cliente
             ui->plainTextEdit->setReadOnly(true);
             ui->plainTextEdit->appendPlainText("Login :"+ QString(buffer));
         xml = QString (buffer)+"\n";
+
+        QString inputL(buffer);
+        QByteArray bytesL = inputL.toUtf8();
+            QDomDocument docL;
+            docL.setContent(bytesL);
+            QDomNodeList startL = docL.elementsByTagName("username");
+            int lengthL = startL.length();
+            int lengthInnerL;
+            for (int i = 0; i < lengthL; i++){
+                auto nodeL = startL.at(i);
+                auto mapL = nodeL.attributes();
+                lengthInnerL = mapL.length();
+                for (int j = 0; j < lengthInnerL; j++){
+                    auto mapItemL = mapL.item(j);
+                    auto attributeL = mapItemL.toAttr();
+                    qDebug() << "Username: " << attributeL.value();
+                }
+        }
+
+            QString inputL2(buffer);
+            QByteArray bytesC2 = inputL2.toUtf8();
+                QDomDocument docL2;
+                docL2.setContent(bytesC2);
+                QDomNodeList startL2 = docL2.elementsByTagName("password");
+                int lengthL2 = startL2.length();
+                int lengthInnerL2;
+                for (int i = 0; i < lengthL2; i++){
+                    auto nodeL2 = startL2.at(i);
+                    auto mapL2 = nodeL2.attributes();
+                    lengthInnerL2 = mapL2.length();
+                    for (int j = 0; j < lengthInnerL2; j++){
+                        auto mapItemL2 = mapL2.item(j);
+                        auto attributeL2 = mapItemL2.toAttr();
+                        qDebug() << "Password: " << attributeL2.value();
+                    }
+            }
+
         tcpcliente[0]->write( xml.toLatin1().data() , xml.toLatin1().size());
         }
     }
@@ -146,7 +175,6 @@ void Odyssey_S::leer_socketLogin() {       //Recibe los datos del cliente
  */
 void Odyssey_S::leer_socketSingin() {       //Recibe los datos del cliente
     if(tcpcliente[1]->bytesAvailable() > 0){
-        qDebug() << "Socket Sing_in ";
         QByteArray buffer;
         buffer.resize( tcpcliente[1]->bytesAvailable());
         tcpcliente[1]->read( buffer.data(),buffer.size() );     //lee los datos del cliente
@@ -155,41 +183,148 @@ void Odyssey_S::leer_socketSingin() {       //Recibe los datos del cliente
             ui->plainTextEdit->appendPlainText("Sing_In > "+ QString(buffer));
         xml = QString (buffer)+"\n";
         xml2= QString(buffer);
+
         QString nada = QString("SI")+"\n";
 
-        QString a1 = "a3";
-        QString a2 = "b3";
-        QString a3 = "c3";
+        QString uSer,fuSer,aSer,pSer,gSer,fSer = "";
 
-       /* QString input("<username key=\""+a1+"\"><fullname key=\""+a2+"\"><password key=\""+a3+"\">");
-                      */
-QString input(buffer);
-
+        QString input(buffer);
         QByteArray bytes = input.toUtf8();
+        QDomDocument doc;
+        doc.setContent(bytes);
+        QDomNodeList start;
+        int length=0;
+        int lengthInner;
+        start = doc.elementsByTagName("username");
+        length = start.length();
 
-            QDomDocument doc;
-            doc.setContent(bytes);
+        for (int i = 0; i < length; i++){
+            auto node = start.at(i);
+            auto map = node.attributes();
+            lengthInner = map.length();
+            for (int j = 0; j < lengthInner; j++){
+                auto mapItem = map.item(j);
+                auto attribute = mapItem.toAttr();
+                qDebug() << "Username: " << attribute.value();
+                uSer=attribute.value();
+            }
+        }
+        start = doc.elementsByTagName("fullname");
+        length = start.length();
+        for (int i = 0; i < length; i++){
+            auto node = start.at(i);
+            auto map = node.attributes();
+            lengthInner = map.length();
+            for (int j = 0; j < lengthInner; j++){
+                auto mapItem = map.item(j);
+                auto attribute = mapItem.toAttr();
+                qDebug() << "Fullname: " << attribute.value();
+                fuSer=attribute.value();
+            }
+        }
 
-            QDomNodeList start = doc.elementsByTagName("password");
-            int length = start.length();
-            int lengthInner;
-            for (int i = 0; i < length; i++)
-            {
-                auto node = start.at(i);
-                auto map = node.attributes();
+        doc.setContent(bytes);
+        start = doc.elementsByTagName("age");
+        length = start.length();
+        for (int i = 0; i < length; i++){
+            auto node = start.at(i);
+            auto map = node.attributes();
+            lengthInner = map.length();
+            for (int j = 0; j < lengthInner; j++){
+                auto mapItem = map.item(j);
+                auto attribute = mapItem.toAttr();
+                qDebug() << "Age: " << attribute.value();
+                aSer=attribute.value();
+            }
 
-                lengthInner = map.length();
-                for (int j = 0; j < lengthInner; j++)
-                {
-                    auto mapItem = map.item(j);
-                    auto attribute = mapItem.toAttr();
-                    qDebug() << "Value: " << attribute.value();
-                }
+        }
 
+        start = doc.elementsByTagName("password");
+        length = start.length();
+        for (int i = 0; i < length; i++){
+            auto node = start.at(i);
+            auto map = node.attributes();
+            lengthInner = map.length();
+            for (int j = 0; j < lengthInner; j++){
+                auto mapItem = map.item(j);
+                auto attribute = mapItem.toAttr();
+                qDebug() << "Password: " << attribute.value();
+                pSer=attribute.value();
+            }
+        }
+
+        start= doc.elementsByTagName("genre");
+        length = start.length();
+
+        for (int i = 0; i < length; i++){
+            auto node = start.at(i);
+            auto map = node.attributes();
+            lengthInner = map.length();
+            for (int j = 0; j < lengthInner; j++){
+                auto mapItem = map.item(j);
+                auto attribute = mapItem.toAttr();
+                qDebug() << "Genre: " << attribute.value();
+                gSer=attribute.value();
+            }
+        }
+
+        start = doc.elementsByTagName("friends");
+        length = start.length();
+        for (int i = 0; i < length; i++){
+            auto node = start.at(i);
+            auto map = node.attributes();
+            lengthInner = map.length();
+            for (int j = 0; j < lengthInner; j++){
+                auto mapItem = map.item(j);
+                auto attribute = mapItem.toAttr();
+                qDebug() << "Friends: " << attribute.value();
+                fSer=attribute.value();
+            }
+        }
+
+        QJsonObject addressObject;
+        addressObject.insert("UserName", uSer);
+        addressObject.insert("Fullname", fuSer);
+        addressObject.insert("Age", aSer);
+        addressObject.insert("Password", pSer);
+
+
+
+        QJsonArray friendArray;
+
+        QStringList list = fSer.split(QRegExp("\\s+"), QString::SkipEmptyParts);
+        qDebug() << "Amigos:";
+        for(int o =0; o<list.length();o++){
+            qDebug() << list[o];
+            friendArray.push_back(list[o]);
+        }
+        addressObject.insert("Friends", friendArray);
+
+        QJsonArray genreArray;
+        qDebug() << "generos:";
+        QStringList list1 = gSer.split(QRegExp("\\s+"), QString::SkipEmptyParts);
+        for(int o =0; o<list1.length();o++){
+            qDebug() << list1[o];
+            genreArray.push_back(list1[o]);
+        }
+        addressObject.insert("Genre", genreArray);
+
+
+
+
+        QJsonDocument docXML(addressObject);
+            qDebug() << docXML.toJson();
+
+
+        std::ofstream userFile(user,std::ostream::app);
+
+        userFile << docXML.toJson().toStdString() << std::endl;
+
+        userFile.close();
         //tcpcliente[1]->write( xml.toLatin1().data() , xml.toLatin1().size()); //envio datos al cliente
         tcpcliente[1]->write( nada.toLatin1().data() , nada.toLatin1().size()); //envio datos al cliente
-            }}
-}
+        }
+    }
     else{
         ui->plainTextEdit->appendPlainText("No se puedo realizar la comunicacion <socket Sing in> ");
     }
@@ -212,7 +347,23 @@ void Odyssey_S::leer_socketcliente() {       //Recibe los datos del cliente
         xml = QString (buffer)+"\n";
 
 
-
+        QString inputC1(buffer);
+        QByteArray bytesC1 = inputC1.toUtf8();
+            QDomDocument doc;
+            doc.setContent(bytesC1);
+            QDomNodeList start = doc.elementsByTagName("password");
+            int length = start.length();
+            int lengthInner;
+            for (int i = 0; i < length; i++){
+                auto node = start.at(i);
+                auto map = node.attributes();
+                lengthInner = map.length();
+                for (int j = 0; j < lengthInner; j++){
+                    auto mapItem = map.item(j);
+                    auto attribute = mapItem.toAttr();
+                    qDebug() << "Value: " << attribute.value();
+                }
+        }
 
 
         /*
